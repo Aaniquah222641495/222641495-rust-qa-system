909695307d2cdd616785e96723459ab54ccf42e9
Aaniquah Dicks <222641495@mycput.ac.za>
2026-02-20 14:45:21 +0200

    Initial project structure and Cargo.toml

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![recursion_limit = "256"]
+
+mod cli;
+mod application;
+mod domain;
+mod data;
+mod ml;
+mod infra;
+
+use anyhow::Result;
+use cli::Cli;
+use clap::Parser;
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("word_doc_qa=info".parse().unwrap()),
+        )
+        .init();
+
+    let cli = Cli::parse();
+    cli.run()
+}
